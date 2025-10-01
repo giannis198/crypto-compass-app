b3cf086c47d1d5dd6269de77e35f68aa82e14a8e
giannis198 <john_nik@windowslive.com>
2025-10-01 21:11:16 +0300

    fix: Disable swipe gesture on stack navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ gestureEnabled: false }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-800">
         Welcome to Nativewind!
       </Text>
