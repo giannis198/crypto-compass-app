298d14eba9c1a3f929fb9275d32a978662f08ac8
giannis198 <john_nik@windowslive.com>
2025-10-01 21:05:10 +0300

    Revert "feat: Add sticky header with logo and title"
    This reverts commit 98fd606d0bd02104993534da9a6dcaf7fd889c7a.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,32 @@
 import "../global.css";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
-import Header from "../components/Header";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+export const unstable_settings = {
+  anchor: "(tabs)",
+};
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Header />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="home" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
       </Stack>
-    </SafeAreaView>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white pt-20">
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-800">
         Welcome to Nativewind!
       </Text>
